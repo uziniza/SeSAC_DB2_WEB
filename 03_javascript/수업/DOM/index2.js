7092db0c1146b147a6a5f91afbb74d782eaf513e
오유진 <geeny5@naver.com>
2024-11-06 17:15:59 +0900

    241106 실습 끝

--- 03_javascript/수업/DOM/index2.js
+++ 03_javascript/수업/DOM/index2.js
@@ -5,7 +5,11 @@
 let div1 = document.getElementById('div1')
 div1.innerText="     여기는 <b>첫번째</b> 태그입니다.&hearts;     /"
 console.log(div1.innerText)
-div1.innerHTML="여기는 <b>첫번째</b> 태그입니다.&hearts;"
-div1.textContent="     여기는 <b>첫번째</b> 태그입니다.&hearts;     /"
+//2칸이상의 공백문자 제거, 앞뒤로 공백문자 제거
+
+div1.innerHTML="여기는 <b>첫번째</b> 태그입니다.&hearts;"//태그를 태그로 읽음
+
+
+div1.textContent="     여기는 <b>첫번째</b> 태그입니다.&hearts;     /"//공백 살아있음
 console.log(div1.textContent)
 
