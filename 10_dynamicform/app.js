f48c432d5849b1650a58475d609b0cc3602e9ae8
오유진 <geeny5@naver.com>
2024-11-22 16:46:08 +0900

    동적폼 배웠다 너무 어려움TㅅT

---
+++ 10_dynamicform/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+// 미들웨어 설정
+// 1. 뷰 폴더 설정
+app.set("view engine", "ejs");
+app.set("views", "./views");
+// 2. body-parser설정
+app.use(express.urlencoded({ extended : false}));
+app.use(express.json())
+//api
+app.get('/', (req,res)=>{
+    res.render("index")
+})
+//ajax Get
+app.get('/ajax',(req,res)=>{
+    console.log(req.qurey);
+    res.send(req.qurey);
+})
+//ajax Post
+//body-parser 설정을 해야 req.body를 읽을 수 있음
+app.post('/ajax',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
+
+//axios Get
+app.get('/axios',(req,res)=>{
+    console.log(req.qurey);
+    res.send(req.qurey)
+})
+
+//axios POST
+app.post('/axios',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+});
+
+//fetch요청 Get
+app.get('/fetch',(req,res)=>{
+    console.log(req.qurey);
+    // res.send("응답완료"); : 클라이언트에서 .text()사용
+    res.send(res.qurey);//객체를 보내기 때문에 클라이언트에서 .json() 사용
+})
+
+//fetch요청 post
+app.post('/fetch',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`)
+})
---
+++ 10_dynamicform/temp/json.js
@@ -0,0 +1,33 @@
+//JSON
+//JavaScript Object Notation
+/*
+{
+    "name":"yujin",
+    "married":false,
+    "family":{"father":"ddd","mother":"mmm"}
+}
+ */
+const car = `{
+    "model":"아이오닉6",
+    "company":"현대",
+    "price":50000000,
+    "year":2023,
+    "isElextric":true,
+    "option":["side mirror","smart sensor"]
+}`
+console.log(typeof car);
+//JSON.parse() = json -> object
+const obj = JSON.parse(car);
+console.log(obj);
+console.log(typeof obj);
+console.log(obj.model);
+console.log(obj.option);
+//JSON.stringfy() = object -> json
+const carJson = JSON.stringify(obj);
+console.log(carJson);
+console.log(typeof carJson);
+
+//stringify()의 세번째 인자 : 들여쓰기할 공백의 크기
+const carJson2 = JSON.stringify(obj, null, 5);
+console.log(carJson2)
+console.log(carJson2.model)
